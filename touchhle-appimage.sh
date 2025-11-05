7d4b7b35cc7cf6c99b729ad50e7c9dca2d7f21d5
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-05 15:27:55 -0400

    package the thing

---
+++ touchhle-appimage.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -eu
+
+ARCH="$(uname -m)"
+VERSION="$(cat ~/version)"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME=TouchHLE-"$VERSION"-anylinux-"$ARCH".AppImage
+export DESKTOP=DUMMY
+export ICON=DUMMY
+export DEPLOY_OPENGL=1
+export DEPLOY_SDL=1
+
+# ADD LIBRARIES
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/touchHLE
+
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
+
+mkdir -p ./dist
+mv -v ./*.AppImage* ./dist
+mv -v ~/version     ./dist
