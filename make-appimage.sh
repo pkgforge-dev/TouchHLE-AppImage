5803a227fe2aef0c846091488ed6da1e752cbfa1
Link4Electronics <link4electronics@gmail.com>
2026-02-21 16:55:13 -0300

    Create make-appimage.sh

---
+++ make-appimage.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=PATH_OR_URL_TO_ICON
+export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export DEPLOY_OPENGL=1
+export DEPLOY_PULSE=1
+
+# Deploy dependencies
+quick-sharun /usr/bin/touchHLE /usr/share/touchhle
+
+# Additional changes can be done in between here
+mkdir -p ./dist
+mv -v ./*.AppImage* ./dist
+mv -v ~/version     ./dist
+
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --test ./dist/*.AppImage
