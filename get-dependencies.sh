61ac31fa864ab80a0cee759fbe718e82920adf6d
Link4Electronics <link4electronics@gmail.com>
2026-02-21 22:46:33 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -56,3 +56,4 @@ export SDL2_CONFIG_PATH=/usr/bin/sdl2-config
 cargo build --release --no-default-features
 mv -v target/release/touchHLE ../AppDir/bin
 mv -v touchHLE_default_options.txt ../AppDir/bin/options.txt
+mv -v touchHLE_fonts ../AppDir/bin
