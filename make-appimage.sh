5d4b66169ae96923a71dd305c2850b95ec609b39
Link4Electronics <link4electronics@gmail.com>
2026-02-23 03:00:09 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,7 +12,6 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/touchHLE #/usr/share/touchhle
-#echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
 
