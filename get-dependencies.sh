8155bcc0edbf2add32681f95110bc209a49321e5
Link4Electronics <link4electronics@gmail.com>
2026-08-15 21:20:32 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,23 +6,12 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	boost	 \
-	cargo	 \
-	cmake	 \
-	libdecor \
-	openal   \
-	rustup	 \
-	sdl2
+pacman -Syu --noconfirm boost cargo cmake openal rustup sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano ! llvm
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini ! llvm
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
 echo "Making stable build of touchHLE..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/touchHLE/touchHLE"
