1ac61f4df88e4d3ecd9733866ce5a5dc4fc8dfa9
Link4Electronics <link4electronics@gmail.com>
2026-08-30 12:57:49 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook:wayland-is-broken.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/touchHLE/touchHLE/refs/heads/trunk/res/icon.png
-export USE_HOST_DRIVERS_EXPERIMENTAL=1
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/touchHLE
